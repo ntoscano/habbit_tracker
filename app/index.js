6a009948d84aaf26aad253aa6d0d2f66b57cdaf0
Nicolas Toscano <nicolas.toscano@gmail.com>
2017-03-13 15:00:32 -0700

    added back localStorage (oops)

--- app/index.js
+++ app/index.js
@@ -11,13 +11,19 @@ import { AppContainer } from 'react-hot-loader';
 import routes from 'Bitmatica/routes';
 import frontpageReducer from 'Bitmatica/containers/Frontpage/reducer';
 
+import {loadState, saveState} from './localStorage';
 
+const persistedState = loadState();
 const store = createStore(
   combineReducers({
     cms: frontpageReducer,
-  })
+  }),
+  persistedState
 );
 
+store.subscribe(() => {
+  saveState(store.getState());
+});
 
 const render = (Component) => {
   ReactDOM.render(
