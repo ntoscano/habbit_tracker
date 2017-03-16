6e67ee12dcda60502cd994a6e27096f9218d50ce
Nicolas Toscano <nicolas.toscano@gmail.com>
2017-03-15 17:59:58 -0700

    now only one save button to log entry

--- app/containers/TaskDetailPage/index.js
+++ app/containers/TaskDetailPage/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import style from './index.scss';
 import {connect, dispatch} from 'react-redux';
 import { Link } from 'react-router-dom';
+import { browserHistory } from 'react-router'
 import TaskDetails from 'Bitmatica/components/TaskDetails';
 import NavBar from 'Bitmatica/components/NavBar';
 import {addToDo, logToDo, addSubTask} from 'Bitmatica/containers/Frontpage/actions';
@@ -9,11 +10,60 @@ import {v4} from 'node-uuid';
 
 
 class TaskDetailPage extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      taskIdsToLog: {},
+      notesForTaskId: {},
+      textForTaskId: {},
+    }
+  }
+
+  onToggleEntry (id) {
+    let currentValue = this.state.taskIdsToLog[id] ? true : false;
+    this.setState({
+      taskIdsToLog: Object.assign({}, this.state.taskIdsToLog,{
+        [id]: !currentValue,
+      }),
+      notesForTaskId: this.state.notesForTaskId,
+    })
+  }
+
+  onChangeNotes (id, notes) {
+    this.setState({
+      taskIdsToLog: this.state.taskIdsToLog,
+      notesForTaskId: Object.assign({}, this.state.notesForTaskId, {
+        [id]: notes,
+      }),
+    })
+    console.log(this.state);
+  }
+
+  saveEntries () {
+    let entries = Object.keys(this.state.taskIdsToLog).filter((key, index) => {
+      return this.state.taskIdsToLog[key];
+    }).map((id, index) => {
+
+      let task = this.props.task.id === id ? this.props.task : this.props.subTasks.filter((task) => {
+        return task.id === this.props.task.id
+      })[0];
+
+      return {
+        id,
+        text: task.text,
+        notes: this.state.notesForTaskId[id],
+      }
+    });
+    this.props.onCLickLogAll(entries);
+    console.log(entries);
+  }
+
   render() {
-    const subTasks = this.props.subTasks.map((task, index) => {
+    const subTasks = [this.props.task].concat(this.props.subTasks).map((task, index) => {
       let notesInput;
       return (
-        <TaskDetails onLogTask={this.props.onClickLog} task={task} key={task.id} />
+        <TaskDetails onLogTask={this.props.onClickLog} task={task} key={task.id} onToggleEntry={(id) => this.onToggleEntry(id)} onChangeNotes={(id, notes) => this.onChangeNotes(id, notes)} />
       );
     });
     let input;
@@ -24,10 +74,13 @@ class TaskDetailPage extends React.Component {
         <div className="card-columns">
           <div className="card">
             <div className="list-group list-group-flush">
-              <TaskDetails onLogTask={this.props.onClickLog} task={this.props.task} />
               {subTasks}
+              <div className="text-right">
+                <button className="btn btn-secondary" type="button" onClick={() => {browserHistory.goBack()}}>Cancel</button>
+                <button className="btn btn-success" type="button" onClick={() => {this.saveEntries();browserHistory.goBack();}}>Save</button>
+              </div>
               <div className="input-group">
-                <input className="form-control" placeholder="New Subtask Name..." ref={node => {
+                <input className="form-control" placeholder="Add subtask..." ref={node => {
                   input = node;
                 }} />
                 <span className="input-group-btn">
@@ -46,12 +99,17 @@ TaskDetailPage.propTypes = {
   id: React.PropTypes.string,
   task: React.PropTypes.object,
   subTasks: React.PropTypes.array,
+
+  onAddEntry: React.PropTypes.func,
+  onRemoveEntry: React.PropTypes.func,
 }
 
 TaskDetailPage.defaultProps = {
   id: "",
   task: {},
   subTasks: [],
+  onAddEntry: undefined,
+  onRemoveEntry: undefined,
 }
 
 const mapStateToProps = (state, ownProps) => {
@@ -78,6 +136,11 @@ const mapDispatchToProps = (dispatch, ownProps) => {
     },
     onClickLog: (id, text, notes) => {
       dispatch(logToDo(id, text, notes));
+    },
+    onCLickLogAll: (entries) => {
+      entries.map((entry, index) => {
+        dispatch(logToDo(entry.id, entry.text, entry.notes));
+      });
     },
   }
 }
