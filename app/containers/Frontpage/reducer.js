adba6c3072338e73906e2f848eb7842d8d2ad3a3
Nicolas Toscano <nicolas.toscano@gmail.com>
2017-03-19 14:28:44 -0700

    clean up

--- app/containers/Frontpage/reducer.js
+++ app/containers/Frontpage/reducer.js
@@ -9,62 +9,31 @@ export default function addToDoReducer(state = initialState, action) {
   switch (action.type) {
 
     case constants.REQUEST_TODOS:
-      // Should update state here to show loading spinner
+      // Should update state to show loading spinner
       return state;
       break;
 
     case constants.RECEIVE_TODOS:
+      // Should update state to hide loading spinner
       return {
         todos: action.todos,
         loggedTodos: state.loggedTodos,
       }
       break;
+
     case constants.POST_TODO:
+      // Should update state to show loading spinner
       return state;
       break;
+
     case constants.RECEIVE_TODO:
+      // Should update state to hide loading spinner
       return {
         todos: state.todos.concat(action.todo),
         loggedTodos: state.loggedTodos,
       }
       break;
 
-    // case constants.ADD_TO_DO:
-    //   fetch('http://localhost:1337/task', {
-    //     method: 'POST',
-    //     body: JSON.stringify({
-    //       name: action.text,
-    //       sticky: action.parentTaskId ? false: true,
-    //     }),
-    //   }).then(response => response.json())
-    //   .then(json => {
-    //     state.todos.concat([{
-    //       id: json.id,
-    //       text: json.name,
-    //       count: json.count,
-    //       parentTaskId: json.parentTaskId,
-    //       createdAt: new Date(json.createdAt),
-    //       updatedAt: new Date(json.updatedAt),
-    //       sticky: json.sticky,
-    //     }]);
-    //   });
-    //   return state;
-    //   break;
-
-    // case constants.ADD_TO_DO:
-    //   return {
-    //       todos: state.todos.concat([{
-    //         id: action.id,
-    //         text: action.text,
-    //         count: 0,
-    //         parentTaskId: action.parentTaskId,
-    //         createdAt: new Date(),
-    //         updatedAt: new Date(),
-    //         sticky: action.parentTaskId ? false : true,
-    //       }]),
-    //       loggedTodos : state.loggedTodos,
-    //     };
-    //   break;
     case constants.LOG_TODO:
       return {
         todos: state.todos.map((todo, index) => {
